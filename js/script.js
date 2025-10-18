244a7aa984c68221df166fad29a74d60c0dd0566
Fatemah Almarhoon <fatemahrauf15@gmail.com>
2025-10-18 08:58:29 +0300

    Add fade-in effect

--- js/script.js
+++ js/script.js
@@ -54,3 +54,12 @@ document.addEventListener("DOMContentLoaded", () => {
         form.reset();
     });
 });
+
+// Scroll Animation (Fade In)
+const faders = document.querySelectorAll('.fade-in');
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) entry.target.classList.add('visible');
+    });
+}, { threshold: 0.2 });
+faders.forEach(el => observer.observe(el));
